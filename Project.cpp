8c77ff34ad9f466fb95bb8b4bda8c5507f0315c9
Carrieann Towne <30992628+ctowne20@users.noreply.github.com>
2018-12-11 10:18:02 -0500

    v2.0

--- Project.cpp
+++ Project.cpp
@@ -31,22 +31,26 @@ It could be altered to tackle problems in the realm of calculus, trigonometry, e
 #include <queue>
 #include <string>
 #include <vector>
-
 using namespace std;
 
-#define problem "x^2 + 3x - 4"
+//EMAIL HELP https://www.emailarchitect.net/easendmail/ex/vc/14.aspx
 
 //Function prototypes
-string steps();
-void answer(string step);
+void answer(queue<string> solution, ofstream& FILE);
+queue<string> practice(queue<string> solution, vector<string>& assignmentVector, int pTracker);
+queue<string> homework(queue<string> solution, vector<string> assignmentVector, int hTracker);
 void fileStuff(ofstream& FILE, queue<string> solution);
 
 int main()
 {
 	queue<string> solution;
+	vector<string> assignmentVector;
 	string fileName, fName, lName, instructor, ID, problemNumber, userProblem;
 	bool option, cont = 0;
+	int practiceTracker = 0, homeworkTracker = 0;
 	//CTextSpeaker voice; //Text to Speech implementation??
+	ifstream assignment;
+	ofstream HFILE, PFILE;
 
 	//Display effects
 	CONSOLE_FONT_INFOEX font;
@@ -61,119 +65,145 @@ int main()
 	//REFERENCES: https://docs.microsoft.com/en-us/windows/console/console-font-infoex
 	//			  https://stackoverflow.com/questions/35382432/how-to-change-the-console-font-size
 
-	do
+	cout << "Welcome to the Mathematics Process Tracker! \n";
+	cout << "Would you like to work on practice problems (enter 0) or homework problems (enter 1)? \n";
+	cin >> option;
+
+	cout << "First Name: ";
+	cin >> fName;
+	cout << "Last Name: ";
+	cin >> lName;
+	cout << "Instructor: ";
+	cin >> instructor;
+	cout << "ID Number: ";
+	cin >> ID;
+
+	if (!option) //Practice
 	{
-		cout << "Welcome to the Mathematics Process Tracker! \n";
-		cout << "Would you like to work on practice problems (enter 0) or homework problems (enter 1)? \n";
-		cin >> option;
+		fileName = "PracticeFile.txt";
+		PFILE.open(fileName);
 
-		cout << "First Name: ";
-		cin >> fName;
-		cout << endl << "Last Name: ";
-		cin >> lName;
-		cout << endl << "Instructor: ";
-		cin >> instructor;
-		cout << endl << "ID Number: ";
-		cin >> ID;
-		cout << endl << "Problem Number: ";
-		cin >> problemNumber;
-
-		if (!option)
+		assignment.open("C:/Users/ctowne/Documents/Visual Studio 2017/practiceProblems.txt");
+		if (!assignment)
 		{
-			ofstream PFILE;
-			fileName = "PracticeFile.txt";
-			PFILE.open(fileName);
+			cout << "Unable to open file" << endl;
+			system("pause");
+			exit(1);
+		}
 
+
+		//Read professor's file into a vector
+		string line;
+		while (getline(assignment, line))
+		{
+			assignmentVector.push_back(line);
+		}
+
+
+		cout << "Practice Assignment: " << endl;
+
+		do 
+		{
+			system("cls");
 			//Print header in output file
-			PFILE << fName << " " << lName << endl << "Professor " << instructor << endl
+			PFILE << "Practice Assignment" << endl << endl << fName << " " << lName << endl << "Professor " << instructor << endl
 				<< "Student Number " << ID << endl << "Problem " << problemNumber << ":" << endl;
 
+			solution = practice(solution, assignmentVector, practiceTracker);
 			fileStuff(PFILE, solution);
-			PFILE.close();
+			answer(solution, PFILE);
 
+			cout << endl << endl << "Would you like to begin another assignment / practice? (Enter 1 to continue, 0 to exit)" << endl;
+			cin >> cont;
 
-		}
-		else if (option)
+		} while (cont);
+
+	}
+	else if (option) //Homework
+	{
+		fileName = "HomeworkFile.txt";
+		HFILE.open(fileName);
+
+		ofstream Grades;
+		string gradefileName = lName + "Grade";
+		Grades.open(gradefileName);
+
+		assignment.open("assignedProblems.txt");
+		if (!assignment)
 		{
-			ofstream HFILE;
-			fileName = "HomeworkFile.txt";
-			HFILE.open(fileName);
-
-			//Print header in output file
-			HFILE << fName << " " << lName << endl << "Professor " << instructor << endl
-				<< "Student Number " << ID << endl << "Problem " << problemNumber << ":" << endl;
-
-			// 2 IMPLEMENTATION OPTIONS...
-			// 1) User inputs problem
-			cout << endl << endl << "Enter problem: ";
-			cin >> userProblem;
-			HFILE << "\t" << userProblem << endl << endl;
-
-			// 2) Program displays problem
-			cout << endl << endl << "Your problem:  " << problem << endl << endl;
-			HFILE << problem << endl << endl;
-
-
-			fileStuff(HFILE, solution);
-			HFILE.close();
-
+			cout << "Unable to open file" << endl;
+			system("pause");
+			exit(1);
 		}
-		else
-		{
-			"Please enter 0 or 1!";
-			cont = 1;
-			continue;
-		}
-		cout << "Would you like to begin another assignment / practice? (Enter 1 to continue, 0 to exit)" << endl;
-		cin >> cont;
 
-	} while (cont);
+		//Print header in output file
+		HFILE << fName << " " << lName << endl << "Professor " << instructor << endl
+			<< "Student Number " << ID << endl << "Problem " << problemNumber << ":" << endl;
+
+		solution = homework(solution, assignmentVector, homeworkTracker);
+		fileStuff(HFILE, solution);
+		answer(solution, HFILE);
+	}
+	else
+	{
+		cout << "Error: You must enter 0 for practice or 1 for homework";
+		system("pause");
+		exit(1);
+	}
 
 	//voice.Speak(Lproblem); //Text to speech implementation??
-
-	//Begin reading in solution
-	solution.push(steps()); //push the string of each step into the stack 
-							//by calling "steps" function
+	HFILE.close();
+	PFILE.close();
+	assignment.close();
 
 	system("pause");
 	return 0;
 }
 
-string steps()
+//print out answer at the end of the File and at the bottom of the screen
+void answer(queue<string> solution, ofstream& FILE)
 {
+	cout << endl <<  "Final Solution: " << endl << solution.back();
+	FILE << endl <<  "Final Solution: " << endl << solution.back();
+}
+
+//read in user data for practice problems
+//If output equals "FINAL", system("cls"), print the next problem
+//keep track of number of problems
+queue<string> practice(queue<string> solution, vector<string>& assignmentVector, int pTracker)
+{
+	pTracker++;
 	string step;
-	bool stop = 0;
 
-	cout << "Solution: " << endl;
-	cin >> step;
+	cout << "Problem: " << assignmentVector.front();
+	assignmentVector.erase(assignmentVector.begin());
+	cout << endl << "Please enter each step of your work. Press enter" 
+		<< " between lines. Enter FINAL to finish the problem." << endl;
 
-	if (step[0] == '*' && step[1] == '*')
-		answer(step);
-
-	return step;
-}
-
-void answer(string step)
-{
-
-
-}
-
-void practice(ofstream PFILE)
-{
-	//print out practice problems
-	//read in user data
-	//if ouput equals "FINAL", system("cls"), print new problem
-	//keep track of number of problems
-
-	int practiceTracker = 0;
-	string read;
-
-	for (int i = 0; i < read.size(); i++)
+	do
 	{
-		PFILE << read[i];
+		cin >> step;
+		solution.push(step);
+	} while (step != "FINAL");
+
+	return solution;
+}
+
+queue<string> homework(queue<string> solution, vector<string> assignmentVector, int hTracker)
+{
+	hTracker++;
+	string step;
+
+	cout << "Please enter each step of your work. Press enter"
+		<< "between lines. Enter FINAL to finish the problem." << endl;
+
+	while (step != "FINAL")
+	{
+		cin >> step;
+		solution.push(step);
 	}
 
+	return solution;
 }
 
 void fileStuff(ofstream& FILE, queue<string> solution)
@@ -185,5 +215,4 @@ void fileStuff(ofstream& FILE, queue<string> solution)
 		solution.pop();
 		FILE << temp << endl;
 	}
-	
 }
